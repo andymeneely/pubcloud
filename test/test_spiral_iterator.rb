979bf45a94fd62656d7e76ccd89cc5a47ed3e38d
Andy Meneely <andy.meneely@gmail.com>
2013-05-24 08:41:10 -0700

    A spiral iterator that's not evenly-spaced

--- test/test_spiral_iterator.rb
+++ test/test_spiral_iterator.rb
@@ -2,7 +2,7 @@ require 'pubcloud/spiral_iterator'
 
 class TestSpiralIterator < Test::Unit::TestCase
 
-  def test_first_is_center
+  def _test_first_is_center
 	itr = SpiralIterator.new({:x=>5,:y=>5}, maxSize=10, numSteps=1)
 	times = 0
 	itr.each do |x,y|
@@ -13,7 +13,7 @@ class TestSpiralIterator < Test::Unit::TestCase
 	assert_equal(1, times, "Iterator went more than one time")
   end
 
-  def _test_a_few_more
+  def test_a_few_more
 	itr = SpiralIterator.new({:x=>5,:y=>5}, maxSize=10, numSteps=6)
 	times = 0
 	act_x = []
@@ -23,6 +23,7 @@ class TestSpiralIterator < Test::Unit::TestCase
 	  act_y << y
 	  times = times + 1
 	end
+	puts "actual x: #{act_x}\nactual y: #{act_y}"
 	assert_equal([5,6,4,2,2,6], act_x)
 	assert_equal([5,6,7,5,2,0], act_y)
 	assert_equal(6, times)
