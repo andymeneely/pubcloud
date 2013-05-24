6579ac1c7b703ba2adfeb330742a7fdc8b0f8813
Andy Meneely <andy.meneely@gmail.com>
2013-05-24 15:18:04 -0700

    Cleanup

--- test/test_spiral_iterator.rb
+++ test/test_spiral_iterator.rb
@@ -1,17 +1,6 @@
 require 'pubcloud/spiral_iterator'
 
 class TestSpiralIterator < Test::Unit::TestCase
-
-  # def test_first_is_center
-	# itr = SpiralIterator.new({:row=>5,:col=>5}, maxSize=10, numSteps=1)
-	# times = 0
-	# itr.each do |row,col|
-		# assert_equal(5, row)
-		# assert_equal(5, col)
-		# times = times + 1
-	# end
-	# assert_equal(1, times, "Iterator went more than one time")
-  # end
   def run_iterator(width, center)
     itr = SpiralIterator.new(width=width, center=center)
 	act = Hash.new
@@ -29,7 +18,6 @@ class TestSpiralIterator < Test::Unit::TestCase
 	assert_equal([5,5,4,4,4,5,6,6,6], act[:row])
 	assert_equal([5,6,6,5,4,4,4,5,6], act[:col])
   end
-
 
   def test_bigger_spiral
     act = run_iterator(width=5, {:row=>5,:col=>5})
