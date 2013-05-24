a553c7116ec09e6a48260f086de8aa483ad0bdf8
Andy Meneely <andy.meneely@gmail.com>
2013-05-24 15:40:42 -0700

    Getting the basic two-by-two down

--- lib/pubcloud/layout_grid.rb
+++ lib/pubcloud/layout_grid.rb
@@ -3,11 +3,24 @@ class LayoutGrid
   def initialize(width,height)
     @width=width
     @height=height
+    @grid = []
+    (1..@height).each do |row|
+      @grid[row] = []
+      (1..@width).each do |col|
+        @grid[row][col] = "&nbsp;"
+      end
+    end
   end
 
   def to_html
     html = ""
-    
+    (1..@height).each do |row|
+      html << "<tr>"
+      (1..@width).each do |col|
+        html << "<td>#{@grid[row][col]}</td>"
+      end
+      html <<"</tr>"
+    end
     "<table>#{html}</table>"
   end
 end
