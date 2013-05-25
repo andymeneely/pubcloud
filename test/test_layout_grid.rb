9f07347b66d51a0ddf4a4ce615cf6d3a021cd9d8
Andy Meneely <andy@se.rit.edu>
2013-05-25 15:02:35 -0400

    Refactoring the tests to be more readable

--- test/test_layout_grid.rb
+++ test/test_layout_grid.rb
@@ -9,6 +9,22 @@ class TestLayoutGrid < Test::Unit::TestCase
 
   def test_two_by_two
     grid = LayoutGrid.new(width=2, height=2)
-    assert_equal("<table><tr><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td></tr></table>", grid.to_html)
+    exp = "<table>#{tr(td("&nbsp;")*2)*2}</table>"
+    assert_equal(exp, grid.to_html)
   end
+  
+  def test_add_multi_row_multi_col
+    grid = LayoutGrid.new(width=3, height=3)
+    exp = "<table>#{tr(td("&nbsp;")*3)*3}</table>"
+    assert_equal(exp, grid.to_html)
+  end
+  
+  def tr(str)
+    "<tr>#{str}</tr>"
+  end
+  
+  def td(str)
+    "<td>#{str}</td>"
+  end
+  
 end
