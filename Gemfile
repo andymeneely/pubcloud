026984c61a4588e9cef96f15c48cec385e4c9e71
Andy Meneely <andy@se.rit.edu>
2013-06-03 13:12:03 -0400

    Starting the command line options

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "http://rubygems.org"
 gem 'fast-stemmer'
 gem 'tokenizer'
 gem 'activesupport'
+gem 'trollop'
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
--- lib/pubcloud.rb
+++ lib/pubcloud.rb
@@ -1,9 +1,36 @@
 require 'pubcloud/word_counter'
+require 'pubcloud/version'
+require 'trollop'
 
-class Pubcloud
+module Pubcloud
+  class Pubcloud
+    attr_reader :opts
 
-  def initialize()
-  end  
-  
-  
+    def initialize(opts)
+      @opts = opts
+    end 
+
+    def freqs
+        file = File.open(@opts[:file]); text = file.read; file.close
+        WordCounter.new(text).frequencies(@opts[:words])
+    end
+  end
+
+  def self.init_via_cli
+    opts = Trollop::options do
+      version "#{VERSION} (c) 2013 Andy Meneely"
+      banner "Make a word cloud of your publications."
+      opt :words, "Number of words to display", :default => 20
+      opt :file, "The source file in plain text", :type => String
+    end
+
+    Trollop::die "No source file given. Use --file" unless opts[:file_given]
+    Trollop::die :words, "must be at least 1" if opts[:words] < 1
+    Trollop::die :file, "must exist" unless File.exist?(opts[:file])
+
+    pc = Pubcloud.new(opts)
+    puts pc.freqs
+  end
+
 end
+
